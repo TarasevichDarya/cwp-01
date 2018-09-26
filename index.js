6ced48da0c82dfeec33252ad84b1c704fd4f1577
Darya <tarasevichdarya@yandex.by>
2018-09-26 20:55:35 +0300

    Print all command line arguments

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 //02 console.log('Hello World');
-// 03
-const name = process.argv[2];
-console.log(`Hi ${name}!`);
 
+// 03
+//const name = process.argv[2];
+//console.log(`Hi ${name}!`);
+
+// 04
+process.argv.forEach(
+    function(element) 
+    {
+        console.log(element);
+    }
+);
 
 
